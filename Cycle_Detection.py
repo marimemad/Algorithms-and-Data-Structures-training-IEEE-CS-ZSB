608b38e26c8c6ba30aa5d1e26978dcdae80cac73
Marim Emad <marimemad110@gmail.com>
2020-11-25 22:11:02 +0200

    Day 5

---
+++ Cycle_Detection.py
@@ -0,0 +1,73 @@
+'''
+A linked list is said to contain a cycle if any node is visited more than once while traversing the list. Given a pointer to the head of a linked list, determine if it contains a cycle. If it does, return 1. Otherwise, return 0.
+
+'''
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the has_cycle function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def has_cycle(head):
+    
+    if head==None: 
+        return(0)
+    
+    fast=head.next
+    while fast:
+        
+        if fast.next==None or head==None:
+            return(0)
+        
+        elif (fast==head):
+            return(1)
+        fast=fast.next.next
+        head=head.next
+        
+    return(0)
+
+if __name__ == '__main__':
---
+++ Flipping_the_Matrix.py
@@ -0,0 +1,48 @@
+'''
+Sean invented a game involving a 2n *2n  matrix where each cell of the matrix contains an integer. He can reverse any of its rows or columns any number of times. The goal of the game is to maximize the sum of the elements in the  submatrix located in the upper-left quadrant of the matrix.
+
+Given the initial configurations for  matrices, help Sean reverse the rows and columns of each matrix in the best possible way so that the sum of the elements in the matrix's upper-left quadrant is maximal. 
+'''
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the flippingMatrix function below.
+def flippingMatrix(matrix):
+    n=len(matrix[0])
+    Sum = 0
+    for i in range(0, n // 2):  
+        for j in range(0, n // 2):  
+          
+            r1, r2 = i, n - i - 1
+            c1, c2 = j, n - j - 1
+             
+            Sum += max(max(matrix[r1][c1], matrix[r1][c2]),  
+                       max(matrix[r2][c1], matrix[r2][c2]))  
+          
+    return Sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        n = int(input())
+
+        matrix = []
+
+        for _ in range(2*n):
+            matrix.append(list(map(int, input().rstrip().split())))
+
+        result = flippingMatrix(matrix)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
