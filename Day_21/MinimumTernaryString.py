c45188180ed51794f74c8f1e4e34356936153d14
Marim Emad <marimemad110@gmail.com>
2021-04-03 19:23:52 +0200

    Day 21

---
+++ Day_21/MinimumTernaryString.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    s=input()
+
+    len1=s.count('1')
+    index2=s.find('2')
+
+    temp=s[:index2]
+
+    zero=temp.count('0')
+    result='0'*zero+'1'*len1+s[index2:].replace('1','')
+
+    print(result)
